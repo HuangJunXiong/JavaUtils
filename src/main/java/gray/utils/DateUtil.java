0d05815a3338463b2bc9ac81fcc7b72d50af068c
HuangJunXiong <923383921@qq.com>
2017-03-31 00:01:19 +0800

    日期工具类提交

--- src/main/java/gray/utils/DateUtil.java
+++ src/main/java/gray/utils/DateUtil.java
@@ -1,6 +1,7 @@
 package gray.utils;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Gray
@@ -10,4 +11,42 @@ public class DateUtil {
 
     private static final SimpleDateFormat DEFAULT_DATE_TIME_FROMAT = new SimpleDateFormat(
             "yyyy-MM-dd HH:mm:ss");
+
+    /**
+     * 获取当前日期字符串（格式为yyyy-MM-dd）
+     * @return
+     */
+    public static String formatTodayDate() {
+        String date = DEFAULT_DATE_FORMAT.format(new Date());
+        return date;
+    }
+
+    /**
+     * 获取当前日期字符串（格式为yyy-MM-dd HH:mm:ss）
+     * @return
+     */
+    public static String formatTodayDateTime() {
+        String date = DEFAULT_DATE_TIME_FROMAT.format(new Date());
+        return date;
+    }
+
+    /**
+     * 按指定格式转换时间字符串（格式为yyyy-MM-dd）
+     * @param myDate
+     * @return
+     */
+    public static String formatDate(Date myDate) {
+        String date = DEFAULT_DATE_FORMAT.format(myDate);
+        return date;
+    }
+
+    /**
+     * 按指定格式转换时间字符串（格式为yyy-MM-dd HH:mm:ss）
+     * @param myDate
+     * @return
+     */
+    public static String formatDateTime(Date myDate) {
+        String date = DEFAULT_DATE_TIME_FROMAT.format(myDate);
+        return date;
+    }
 }
