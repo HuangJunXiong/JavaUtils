04a1a0eddf4de79e7cd96b72ccf4973a28365c15
HuangJunXiong <923383921@qq.com>
2017-03-30 23:45:45 +0800

    日期工具类提交

---
+++ src/main/java/gray/utils/DateUtil.java
@@ -0,0 +1,13 @@
+package gray.utils;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by Gray
+ */
+public class DateUtil {
+    private static final SimpleDateFormat DEFAULT_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    private static final SimpleDateFormat DEFAULT_DATE_TIME_FROMAT = new SimpleDateFormat(
+            "yyyy-MM-dd HH:mm:ss");
+}
