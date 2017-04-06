28dcac116504df2e85ee0e26ce6c4c46376399dd
HuangJunXiong <923383921@qq.com>
2017-04-06 23:34:21 +0800

    字符串工具类提交

--- src/main/java/gray/utils/StringUtil.java
+++ src/main/java/gray/utils/StringUtil.java
@@ -1,6 +1,9 @@
 package gray.utils;
 
+import org.apache.commons.lang.StringUtils;
+
 import java.text.MessageFormat;
+import java.util.regex.Pattern;
 
 /**
  * 字符串工具类
@@ -16,5 +19,16 @@ public class StringUtil {
     public static String message(String message, Object... variables) {
         String output = MessageFormat.format(message, variables);
         return output;
+    }
+
+    /**
+     * 判断当前字符串是否为数字
+     * @param value
+     * @return
+     */
+    public static boolean isNumber(String value) {
+        if (StringUtils.isEmpty(value)) return false;
+        Pattern pattern = Pattern.compile("[0-9]*");
+        return pattern.matcher(value).matches();
     }
 }
